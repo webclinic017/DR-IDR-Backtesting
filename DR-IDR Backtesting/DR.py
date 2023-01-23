c7281f15377fd8d2d505436419940d24e26df741
ChrisIsTaken <s4004634@ba-sachsen.de>
2023-01-23 13:18:40 +0100

    Made several changes to the code. Found many errors and a lot of logical problems.

--- DR-IDR Backtesting/DR.py
+++ DR-IDR Backtesting/DR.py
@@ -20,23 +20,27 @@ class DR(bt.Strategy):
 
         #RDR Vars
         self.rdr_session_vars = {
-            'session_name': 'RDR',
-            'session_hours': [930, 1030, 1300],   #Var defining start of session, end of session, and end of validity of session
-            'dr_high': None,                #Var to store dr high
-            'dr_high_timestamp': None,      #dr high timestamp
-            'dr_low': None,                 #Var to store dr low
-            'dr_low_timestamp': None,       #dr low timestamp
-            'idr_high': None,               #Var to store idr high
-            'idr_high_timestamp': None,
-            'idr_low': None,                #Var to store idr low
-            'idr_low_timestamp': None,
-            'levelbreaks': [],              #Variable to store all the breaks of relevant levels
+            'session_name': 'RDR',                          #Var to store the sessions name
+            'defining_hour_start': datetime.time(9, 30),    #Var to store time when session's defining hour starts
+            'defining_hour_end': datetime.time(10, 30),     #Var to store time when session's defining hour ends
+            'session_validity': datetime.time(13, 00),      #Var to store time when session is invalid
+            'dr_high': None,                                #Var to store dr high
+            'dr_high_timestamp': None,                      #dr high timestamp
+            'dr_low': None,                                 #Var to store dr low
+            'dr_low_timestamp': None,                       #dr low timestamp
+            'idr_high': None,                               #Var to store idr high
+            'idr_high_timestamp': None,             
+            'idr_low': None,                                #Var to store idr low
+            'idr_low_timestamp': None,              
+            'levelbreaks': [],                              #Variable to store all the breaks of relevant levels
             }
 
         #ODR Vars
         self.odr_session_vars = {
             'session_name': 'ODR',
-            'session_hours': [300, 430, 200],
+            'defining_hour_start': datetime.time(3, 00),
+            'defining_hour_end': datetime.time(4, 30),
+            'session_validity': datetime.time(2, 00),
             'dr_high': None,
             'dr_high_timestamp': None,
             'dr_low': None,
@@ -51,7 +55,9 @@ class DR(bt.Strategy):
         #ADR Vars
         self.adr_session_vars = {
             'session_name': 'ADR',
-            'session_hours': [1930, 2030, 830],
+            'defining_hour_start': datetime.time(19, 30),
+            'defining_hour_end': datetime.time(20, 30),
+            'session_validity': datetime.time(8, 30),
             'dr_high': None,
             'dr_high_timestamp': None,
             'dr_low': None,
@@ -70,53 +76,61 @@ class DR(bt.Strategy):
     
     def next(self):
         print("Reached next()")
+        current_time = data.datetime.time()
+
+        print("Checking if current_time is in correct format", current_time)
         #For every session check if hour has passed or not
+        print("look how output of defining hour end looks like", datetime.time(10, 30))
         for session_variables in [self.rdr_session_vars, self.odr_session_vars, self.adr_session_vars]:
-                hour = int(session_variables['session_hours'][1]) % 12 if int(session_variables['session_hours'][1]) > 12 else int(session_variables['session_hours'][1])
-                # Check if the session's defining hour has passed
-                if self.data.datetime.time().hour > hour:
-                    print("Defining hour has passed")
-                    #Check if session is valid
-                    if self.data.datetime.time().hour < int(session_variables['session_hours'][2]):
-                        print("Session is valid, checking for breaks in any relevant levels")
-                        #Check for breaks in DR and IDR Levels and store them in list
-                        #following code is supposed to be in session loop
-                        class breakdirection(Enum):
-                            BROKE_BELOW = 1
-                            BROKE_ABOVE = 2
-
-                        def breaklevel(open_price, close_price, level):
-                            if open_price <= level <= close_price:
-                                return breakdirection.BROKE_BELOW
-                            elif open_price >= level >= close_price:
-                                return breakdirection.BROKE_ABOVE
-
-                            levels = [session_variables['dr_low'], session_variables['idr_low'], session_variables['dr_high'], session_variables['idr_high']]
-                            open_price, close_price = self.data.open[0], self.data.close[0]
-
-                            for level in levels:
-                                result = breaklevel(open_price, close_price, level)
-                                session_variables['levelbreaks'].append(session_variables, self.data.datetime.time(), level, result, open_price, close_price, levels)
-                    else:
-                        #Session is not valid; append variables to csv
-                        print("Session is not valid, adding relevant data to csv")
-                        self.csvwriter.writerow([session_variables['session_name'], session_variables['dr_high'], session_variables['dr_high_timestamp'], session_variables['dr_low'], session_variables['dr_low_timestamp'], session_variables['idr_high'], session_variables['idr_high_timestamp'], session_variables['idr_low'], session_variables['idr_low_timestamp'], session_variables['levelbreaks']])
-                        self.csvfile.flush()
-
+            print("reached for loop")
+            print("Print session name that is being checked for iteration of loop: ", session_variables['session_name'])
+            print("Check what the sessions timestamp outputs are; dhs, dhe, sv:", session_variables['defining_hour_start'], session_variables['defining_hour_end'], session_variables['session_validity'])
+            print("Check if defining hour is still ongoing")
+            if (current_time > session_variables['defining_hour_end']) and (current_time <= session_variables['session_validity']):
+                print("Defining hour has passed")
+                #Check if session is valid
+                if (current_time > session_variables['defining_hour_end']) and (current_time <= session_variables['session_validity']):
+                    print("Session is valid, checking for breaks in any relevant levels")
+                    #Check for breaks in DR and IDR Levels and store them in list
+                    #following code is supposed to be in session loop
+                    class breakdirection(Enum):
+                        BROKE_BELOW = 1
+                        BROKE_ABOVE = 2
+                    def breaklevel(open_price, close_price, level):
+                        if open_price <= level <= close_price:
+                            return breakdirection.BROKE_BELOW
+                        elif open_price >= level >= close_price:
+                            return breakdirection.BROKE_ABOVE
+                        levels = [session_variables['dr_low'], session_variables['idr_low'], session_variables['dr_high'], session_variables['idr_high']]
+                        open_price, close_price = self.data.open[-1], self.data.close[-1]
+                        for level in levels:
+                            result = breaklevel(open_price, close_price, level)
+                            session_variables['levelbreaks'].append(session_variables, current_time, level, result, open_price, close_price, levels)
                 else:
-                    print("Defining hour is ongoing; update prices")
+                    #Session is not valid; append variables to csv
+                    print("Session is not valid, adding relevant data to csv")
+                    print(session_variables['session_name'], session_variables['dr_high'], session_variables['dr_high_timestamp'], session_variables['dr_low'], session_variables['dr_low_timestamp'], session_variables['idr_high'], session_variables['idr_high_timestamp'], session_variables['idr_low'], session_variables['idr_low_timestamp'], session_variables['levelbreaks'])
+                    self.csvwriter.writerow([session_variables['session_name'], session_variables['dr_high'], session_variables['dr_high_timestamp'], session_variables['dr_low'], session_variables['dr_low_timestamp'], session_variables['idr_high'], session_variables['idr_high_timestamp'], session_variables['idr_low'], session_variables['idr_low_timestamp'], session_variables['levelbreaks']])
+                    self.csvfile.flush()
+            else:
+                print("Defining hour is ongoing, check again and then update prices")
+                if (current_time >= session_variables['defining_hour_start'] and (current_time <= session_variables['defining_hour_end'])):
+                    print("update prices2")
                     #update prices if session's defining hour is still ongoing
                     #update dr's
-                    if self.data.close[0] > session_variables['dr_high']:
-                        session_variables['dr_high'] = self.data.close[0]
-                    if self.data.close[0] < session_variables['dr_low']:
-                        session_variables['dr_low'] = self.data.close[0]
+                    #if (a is None) or (a > b):
+                    if (session_variables['dr_high'] == None) or (self.data.close[-1] > session_variables['dr_high']):
+                        session_variables['dr_high'] = self.data.close[-1]
+                    if (session_variables['dr_low'] == None) or (self.data.close[-1] < session_variables['dr_low']):
+                        session_variables['dr_low'] = self.data.close[-1]
                     
                     #update idr's
-                    if self.data.high[0] > session_variables['idr_high']:
-                        session_variables['idr_high'] = self.data.high[0]
-                    if self.data.high[0] > session_variables['idr_high']:
-                        session_variables['idr_high'] = self.data.high[0]
+                    if (session_variables['idr_high'] == None) or (self.data.high[-1] > session_variables['idr_high']):
+                        session_variables['idr_high'] = self.data.high[-1]
+                    if (session_variables['idr_low'] == None) or (self.data.low[-1] > session_variables['idr_low']):
+                        session_variables['idr_low'] = self.data.low[-1]
+
+                        print("Updated values: drhigh:", session_variables['dr_high'], "drlow: ", session_variables['dr_low'], "idrhigh: ", session_variables['idr_high'], "idrlow: ", session_variables['idr_low'])
 
 if __name__ == '__main__':
     # Create a cerebro entity
@@ -129,17 +143,27 @@ if __name__ == '__main__':
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, '.\data\GBPJPY=X.csv')
+    #date,time,open,high,low,close,volume(which is always 0)
+    datapath = os.path.join(modpath, '.\data\datasampleforruntimereduction.csv')
 
     # Create a Data Feed
-    data = bt.feeds.YahooFinanceCSVData(
+    data = bt.feeds.GenericCSVData(
         dataname=datapath,
         # Do not pass values before this date
         fromdate=datetime.datetime(2000, 1, 1),
-        # Do not pass values before this date
-        todate=datetime.datetime(2022, 12, 31),
         # Do not pass values after this date
-        reverse=False)
+        todate=datetime.datetime(2022, 12, 31),
+       
+        dtformat=('%Y.%m.%d'),
+        tmformat=('%H:%M'),
+
+        datetime=0,
+        time=1,
+        open=2,
+        high=3,
+        low=4,
+        close=5,
+        volume=6)
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
